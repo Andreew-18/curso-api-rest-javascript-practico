c071b982391150b85ed191147c8f900ae3d251a1
Andreew-18 <andreewchaverragonzalez@gmail.com>
2023-08-29 22:20:31 -0500

    Agregandole eventos a los botones

--- src/main.js
+++ src/main.js
@@ -1,1 +1,61 @@
-console.log("API_URL = https://jdjadjsdasd.com/dewidjewdew?api_key" + API_KEY);
+
+const API = axios.create({
+    baseURL : 'https://api.themoviedb.org/3/',
+    headers : {
+        'Content-type' : 'application/json;charset=utf-8',
+    },
+    params : {
+        'api_key' : API_KEY,
+    },
+});
+
+async function getTrendingMoviesPreview(){
+    const {data} = await API(`trending/movie/day`);
+    
+    const movies = data.results;
+    console.log(data);
+    console.log(movies);
+
+    movies.forEach(movie => {
+        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+
+        movieContainer.appendChild(movieImg);
+        trendingMoviesPreviewList.appendChild(movieContainer);
+    });
+}
+// -----------------------------------------------------------------------
+// Categorias
+
+async function getCategoriesPreview(){
+    const {data} = await API(`genre/movie/list`);
+    const categories = data.genres;
+    console.log(data);
+    
+
+    categories.forEach(category => {
+        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name)
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreviewList.appendChild(categoryContainer);
+    });
+}
+
